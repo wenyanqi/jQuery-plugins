dd19d002e2e0d234af0cac50ec0d1e5188591584
wenyanqi <wenyanqi123@gmail.com>
2016-03-28 22:16:49 +0800

    完成了init

--- pictureSlider/plugins/pictureSlider-plugins.js
+++ pictureSlider/plugins/pictureSlider-plugins.js
@@ -4,25 +4,57 @@
 
 	var interval ;
 
-	var defaultOptions = {
-		'animate': 0,  //0--show，hide； 1--fadeIn,fadeOut 2--slideDown,slideUp  3--animate
+	var d_options = {
+		'animate': 3,  //0--show，hide； 1--fadeIn,fadeOut 2--slideDown,slideUp  3--animate
 		'imageNum': 4,
 		'imageWidth': 600,
-		'time': 1000,
+		'time': 2000,
 		'current': 0
 	};
 
-	var numList ;
+	var numList;
+
+	function prev() {
+
+	}
 
 	var methods = {
 		init: function() {
 		
-			// var $this = $(this);
-			// var defaultOptions.imageNum = $this.children("li").length;
-			// var defaultOptions.imageWidth = $this.css("width");
+			var $this = $(this);
+			var lists = $this.children("li");
 
-			// var setting = $.extend($.fn.slider.defaultOptions,defaultOptions, options);
-			
+			d_options.imageNum = $this.children("li").length;
+			d_options.imageWidth = parseInt($this.children("li").css("width"));
+
+			var lastImg = document.createElement("li");
+			var firstImg = document.createElement("li");
+			lastImg.innerHTML = lists[d_options.imageNum-1].innerHTML;
+			firstImg.innerHTML = lists[0].innerHTML;
+			$this.append(firstImg);
+			$this.prepend(lastImg);
+
+			//创建下面的序号1,2,3,4
+			var numberElement = document.createElement("ul");
+			numberElement.className = "number";
+			for(var  i=1; i<=d_options.imageNum;i++) {
+				var liElement = document.createElement("li");
+				liElement.innerHTML = i;
+				numberElement.appendChild(liElement);
+			}
+			numberElement.firstChild.className = "current";
+
+			//numberElement = $this.after(numberElement);
+
+			//创建左右按钮
+			var prevElement = document.createElement("div");
+			prevElement.className = "prev";
+			prevElement.innerHTML = "&lt;";
+			var nextElement = document.createElement("div");
+			nextElement.className = "next";
+			nextElement.innerHTML = "&gt;";
+
+			$this.after(numberElement,prevElement,nextElement);
 
 			return $(this);
 		},
@@ -30,52 +62,56 @@
 			$this = $(this);
 			var imageList = $this.children("li");
 			numList = $(".number").children("li");
-			var options_new = $.extend(defaultOptions, options);
-			if(options_new.current >= options_new.imageNum) {
-				alert("图片总数"+options_new.imageNum+" <=" + "当前图片下标" +current);
+
+			d_options = $.extend(d_options, options);
+			if(d_options.current >= d_options.imageNum) {
+				alert("图片总数"+d_options.imageNum+" <=" + "当前图片下标" +current);
 			}
 
 			//判断是不是已经start过
 			if(interval==undefined) {
 				
-				//$this.css("margin-left","-"+options_new.current*options_new.imageWidth+"px");
+				//$this.css("margin-left","-"+d_options.current*d_options.imageWidth+"px");
 				interval = setInterval(function(){
 
-					// imageList.eq(options_new.current).hide();
-					// imageList.eq(options_new.current).slideUp();
-					numList.eq(options_new.current).toggleClass("current");
-					if(options_new.animate == 0) {
-						imageList.eq(options_new.current).hide();	
-					} else if(options_new.animate == 1) {
-						imageList.eq(options_new.current).fadeOut();
-					} else if(options_new.animate == 2) {
-						imageList.eq(options_new.current).slideUp();
+					// imageList.eq(d_options.current).hide();
+					// imageList.eq(d_options.current).slideUp();
+					numList.eq(d_options.current).toggleClass("current");
+					if(d_options.animate == 0) {
+						imageList.eq(d_options.current).hide();	
+					} else if(d_options.animate == 1) {
+						imageList.eq(d_options.current).fadeOut();
+					} else if(d_options.animate == 2) {
+						imageList.eq(d_options.current).slideUp();
 					} 
 
-					//alert(options_new.current);
-					options_new.current ++;
-					//alert(options_new.current);
-					if(options_new.animate == 0) {
-						imageList.eq(options_new.current).show();	
-					} else if(options_new.animate == 1) {
-						imageList.eq(options_new.current).fadeIn();
-					} else if(options_new.animate == 2) {
-						imageList.eq(options_new.current).slideDown();
+					
+					d_options.current ++;
+					
+					if(d_options.animate == 0) {
+						imageList.eq(d_options.current).show();	
+					} else if(d_options.animate == 1) {
+						imageList.eq(d_options.current).fadeIn();
+					} else if(d_options.animate == 2) {
+						imageList.eq(d_options.current).slideDown();
 					} else {
-						$this.animate({"margin-left": "-"+(options_new.current+1)*options_new.imageWidth+"px"},
+						
+						$this.animate({"margin-left": "-"+(d_options.current+1)*d_options.imageWidth+"px"},
 							{duration:'slow',complete:function(){
-								if(options_new.current == options_new.imageNum) {
-									options_new.current = 0;
+								
+								if(d_options.current == d_options.imageNum) {
+									d_options.current = 0;
 									
-									$this.css("margin-left","-"+(options_new.current+1)*options_new.imageWidth+"px");
-									numList.eq(options_new.current).toggleClass("current");
+									$this.css("margin-left","-"+(d_options.current+1)*d_options.imageWidth+"px");
+									numList.eq(d_options.current).toggleClass("current");
 								}
 							}
 						});
+						
 					}
-					numList.eq(options_new.current).toggleClass("current");
+					numList.eq(d_options.current).toggleClass("current");
 					
-				},options_new.time);	
+				},d_options.time);	
 			}
 			
 			return $(this);
@@ -86,14 +122,37 @@
 			interval = undefined;
 		},
 		changeCurrent: function(num) {
+			numList.eq(d_options.current).toggleClass("current");
+			d_options.current = num;
 			
-			defaultOptions.current = num;
+			$this.css("margin-left","-"+(d_options.current+1)*d_options.imageWidth+"px");
+			numList.eq(d_options.current).toggleClass("current");
 
+		},
+		prev: function(num) {
+			numList.eq(d_options.current).toggleClass("current");
+			if(d_options.current == 0) {
+				d_options.current = d_options.imageNum-1;
+			}else {
+				d_options.current --;
+			}
+			$this.css("margin-left","-"+(d_options.current+1)*d_options.imageWidth+"px");
+			numList.eq(d_options.current).toggleClass("current");
+		},
+		next: function(num) {
+			numList.eq(d_options.current).toggleClass("current");
+			if(d_options.current == d_options.imageNum-1) {
+				d_options.current = 0;
+			}else {
+				d_options.current ++;
+			}
+			$this.css("margin-left","-"+(d_options.current+1)*d_options.imageWidth+"px");
+			numList.eq(d_options.current).toggleClass("current");
 		}
 
 
 	};
-
+ 
 	$.fn.slider = function (method) {
 		//根据传入参数method的不同，调用不同的方法
 		if(methods[method]) {
@@ -107,4 +166,3 @@
 	
 })(jQuery);
 
-	
